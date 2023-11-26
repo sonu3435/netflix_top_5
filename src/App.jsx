75333a592ea1cb61b1de3f1ee746accc03d02be9
Arun <arun@technative.in>
2023-11-26 18:08:51 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Card from './Cards'
+import './index.css'
+import sdata from './sdata'
+
+function App(){
+    return (
+    <>
+        <h1 className="heading_style">List of top 5 Netflix Series...</h1>
+
+        {sdata.map( (val) => {    
+            return(     
+            <Card imgsrc = {val.imgsrc}
+            title = {val.title}
+            sname = {val.sname}
+            link =  {val.link}/>
+            )})}
+
+        
+    </>
+    )
+}
+
+
+export default App;
---
+++ src/sdata.jsx
@@ -0,0 +1,34 @@
+const sdata = [
+    {
+        imgsrc : "https://wallpapercave.com/wp/wp4056410.jpg",
+        title : " A Netflix Original Series",
+        sname : " DARK ",
+        link : "https://www.netflix.com/in/title/80990668?source=35"
+    },
+    {
+        imgsrc : "https://www.heavenofhorror.com/wp-content/uploads/2020/04/Extracurricular-netflix-review.jpg",
+        title : " A Netflix Original Series",
+        sname : " DARK ",
+        link : "https://www.netflix.com/in/title/80990668?source=35"
+    },
+    {
+        imgsrc : "https://wallpapercave.com/wp/wp1917154.jpg",
+        title : " A Netflix Original Series",
+        sname : " Stranger Things ",
+        link : "https://www.netflix.com/in/title/80990668?source=35"
+    },
+    {
+        imgsrc : "https://wallpapercave.com/wp/wp1917154.jpg",
+        title : " A Netflix Original Series",
+        sname : " Stranger Things ",
+        link : "https://www.netflix.com/in/title/80990668?source=35"
+    },
+    {
+        imgsrc : "https://wallpapercave.com/wp/wp1917154.jpg",
+        title : " A Netflix Original Series",
+        sname : " Stranger Things ",
+        link : "https://www.netflix.com/in/title/80990668?source=35"
+    }
+]
+
+export default sdata;
